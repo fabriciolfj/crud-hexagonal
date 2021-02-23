f9a4c577706e801d1e9b9607d2f43896800a779a
fabricio <fabricio.jacob>
2021-02-22 23:53:47 -0300

    add

--- src/main/java/com/github/fabriciolfj/crudperson/core/personcrud/domain/Person.java
+++ src/main/java/com/github/fabriciolfj/crudperson/core/personcrud/domain/Person.java
@@ -1,12 +1,10 @@
 package com.github.fabriciolfj.crudperson.core.personcrud.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public abstract class Person {
 
